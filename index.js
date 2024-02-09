6336f7106d8a1b8cceb1aa8875ddb5d33366595c
AabharanGupta <aabharangupta1001@gmail.com>
2024-02-09 10:08:38 +0530

    ss

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.use(express.json());
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'https://iste-registeration-backend.vercel.app');
+    res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
     // Add other CORS headers as needed
     next();
   });
